f35fcc25ddd1a6ed190f20e90f2fb74d666335d9
Mark <markkamamia@gmail.com>
2024-04-23 15:26:06 +0300

    complete lab

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,6 +3,7 @@ import { render, screen } from "@testing-library/react";
 
 import App from "../components/App";
 
+
 test("renders without errors", () => {
   expect(() => render(<App />)).not.toThrow();
 });
@@ -10,11 +11,6 @@ test("renders without errors", () => {
 test("renders the correct child components", () => {
   const { container } = render(<App />);
 
-  /*
-    Uncomment the line below to see the DOM elements being returned
-    by the App component in your terminal when you run the tests
-  */
-  // screen.debug();
 
   expect(container.querySelector("nav")).toBeInTheDocument();
   expect(container.querySelector("#home")).toBeInTheDocument();
